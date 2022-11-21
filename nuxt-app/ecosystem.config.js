4f4a957e2888ee7a16e77d5ac97d1a8681511f90
bams vpswindows <bbg2021@gmail.com>
2022-11-21 00:29:31 -0800

    fix

---
+++ nuxt-app/ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [{
+    script: '.output/server/index.mjs',
+    env: {
+            "PORT": 3005,
+            "NODE_ENV": "development"
+        },
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
