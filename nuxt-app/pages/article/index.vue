cfd75d4f5d726fb4b999994ea0dee02bd27a8c1f
Bams <bbg2021@gmail.com>
2022-11-23 05:34:53 +0800

    #1 adding google tag analityc

---
+++ nuxt-app/pages/article/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <TheHeader/>
+  <div class="container mx-auto max-w-6xl md:py-16 pt-16 lg:pt-24">
+    <h1 class="text-center text-3xl font-bold underline">
+      Article
+    </h1>
+  </div>
+
+</template>
